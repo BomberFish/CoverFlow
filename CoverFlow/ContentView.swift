4a127a6f1e772535898b610b8745d31c21a6e5cd
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-03-10 18:24:52 -0500

    not copied :trollface:

--- CoverFlow/ContentView.swift
+++ CoverFlow/ContentView.swift
@@ -9,13 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 0) {
+                ForEach(1..<20) { num in
+                    VStack {
+                        GeometryReader { geo in
+                            Text("Number \(num)")
+                                .font(.largeTitle)
+                                .padding()
+                                .background(.red)
+                                .rotation3DEffect(.degrees(-Double(geo.frame(in: .global).minX) / 8), axis: (x: 0, y: 1, z: 0))
+                                .frame(width: 200, height: 200)
+                        }
+                        .frame(width: 200, height: 200)
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
