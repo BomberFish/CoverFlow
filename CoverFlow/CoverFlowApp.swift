fc0f08c0b3b7832f0c97bbfed4e5c4b863dff4b8
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-03-10 18:10:51 -0500

    Initial Commit

---
+++ CoverFlow/CoverFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoverFlowApp.swift
+//  CoverFlow
+//
+//  Created by Hariz Shirazi on 2023-03-10.
+//
+
+import SwiftUI
+
+@main
+struct CoverFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
